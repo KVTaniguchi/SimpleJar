abb07a3d31f91df8e32c5ddda5111071435f3e36
Kevin Taniguchi <ktaniguchi@urbn.com>
2015-08-29 16:06:55 -0400

    refactored maps
    for loops instead of map

---
+++ Pods/Target Support Files/URBNConvenience/URBNConvenience-umbrella.h
@@ -0,0 +1,19 @@
+#import <UIKit/UIKit.h>
+
+#import "NSDate+URBN.h"
+#import "NSNotificationCenter+URBN.h"
+#import "NSString+URBN.h"
+#import "UIImage+ImageEffects.h"
+#import "UIImage+URBN.h"
+#import "UITextField+URBNLoadingIndicator.h"
+#import "UIView+URBNAnimations.h"
+#import "UIView+URBNBorders.h"
+#import "UIView+URBNLayout.h"
+#import "URBNConvenience.h"
+#import "URBNFunctions.h"
+#import "URBNMacros.h"
+#import "URBNTextField.h"
+
+FOUNDATION_EXPORT double URBNConvenienceVersionNumber;
+FOUNDATION_EXPORT const unsigned char URBNConvenienceVersionString[];
+
